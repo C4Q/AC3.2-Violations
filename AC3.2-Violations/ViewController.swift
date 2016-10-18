d6afc2da5ee064bc9550894850e182dd21aaabee
Kadell <Kadell@MacBook.local>
2016-10-17 21:11:00 -0400

    Resubmit

--- AC3.2-Violations/ViewController.swift
+++ AC3.2-Violations/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var boroLabel: UILabel!
+    @IBOutlet weak var cuisineLabel: UILabel!
+    @IBOutlet weak var DBALabel: UILabel!
+    @IBOutlet weak var violationDescriptionLabel: UILabel!
+    @IBOutlet weak var criticalStatusView: UIView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var inspectionTypeLabel: UILabel!
+    @IBOutlet weak var inspectionDateLabel: UILabel!
+    
+    var theViolations: Violations?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        criticalStatusView.layer.cornerRadius = 25
+        criticalStatusView.layer.masksToBounds = true
+            setupData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupData() {
+        if let violate = theViolations {
+            boroLabel.text = "Borough: \(violate.boro)"
+            scoreLabel.text = violate.score
+            DBALabel.text = violate.name
+            cuisineLabel.text = violate.cuisineDescription
+            violationDescriptionLabel.text = violate.violationDescription
+            inspectionDateLabel.text = violate.inspectionDate
+            inspectionTypeLabel.text = violate.inspectionType
+            if violate.criticalLevel == "Critical" {
+                criticalStatusView.backgroundColor = .red
+                scoreLabel.textColor = .white
+            } else {
+                criticalStatusView.backgroundColor = .yellow
+                
+            }
+            
+        }
     }
-
+    
+    @IBAction func scoreLabelTapped(_ sender: UITapGestureRecognizer) {
+        if let violate = theViolations {
+            if scoreLabel.text == violate.violationCode {
+                scoreLabel.text = violate.score
+            } else {
+                scoreLabel.text = violate.violationCode
+            }
+        }
+    }
+    
 
 }
 
--- AC3.2-Violations/ViolationsTableViewController.swift
+++ AC3.2-Violations/ViolationsTableViewController.swift
@@ -9,15 +9,12 @@
 import UIKit
 
 class ViolationsTableViewController: UITableViewController {
-
+    
+    var allViolations = [Violations]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,71 +22,66 @@ class ViolationsTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func loadData() {
+        guard let path = Bundle.main.path(forResource: "violations", ofType: "json"),
+            let jsonData = try? Data(contentsOf: URL(fileURLWithPath: path), options:  NSData.ReadingOptions.mappedIfSafe),
+            let violationsJSON = try? JSONSerialization.jsonObject(with: jsonData as Data, options: .allowFragments) as? NSArray else {
+                return
+        }
+        
+        if let violations = violationsJSON as? [[String:Any]] {
+            for violationDict in violations {
+                if let ep = Violations(withDict: violationDict) {
+                    self.allViolations.append(ep)
+                }
+            }
+        }
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return allViolations.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let violation = allViolations[indexPath.row]
+        cell.textLabel?.text = violation.name
+        cell.detailTextLabel?.text = violation.criticalLevel
 
         return cell
     }
-    */
+    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+      
+        if segue.identifier  == "segueToDetail", let violation = sender as? Violations {
+            
+            let violate = segue.destination as! ViewController
+            violate.theViolations = violation
+            
+            
+        }
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedViolation = allViolations[indexPath.row]
+        performSegue(withIdentifier: "segueToDetail", sender: selectedViolation)
+    }
+    
 
 }
